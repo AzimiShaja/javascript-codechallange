88cb4b9f76787f8086702648702267c1236d1ab5
Ahmad Shaja AZIMI <110715621+AzimiShaja@users.noreply.github.com>
2023-06-16 18:01:49 +0300

    Added a object and a funtion

--- boolean & if-else/index.js
+++ boolean & if-else/index.js
@@ -1,3 +1,10 @@
+const score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+
 function pickComputerMove(){
     const randomMove = Math.floor(Math.random() * 3 + 1);
 
@@ -21,38 +28,52 @@ function checkWinner(playerMove){
 
         if(cpuMove === 'rock'){
             result = 'Its Tie'
+            score.ties++;
 
         } else if (cpuMove === 'paper'){
             result = 'You Lost to cpu';
+            score.losses++;
 
         } else if (cpuMove === 'scissors'){
             result = 'You won'
+            score['wins']++;
         }
     } else if (playerMove === 'paper'){
 
         if(cpuMove === 'rock'){
             result = 'You won'
+            score.wins++;
 
         } else if (cpuMove === 'paper'){
             result = 'Its tie';
+            score.ties++;
 
         } else if (cpuMove === 'scissors'){
             result = 'You lost to cpu'
+            score.losses++;
         } 
     } else if (playerMove === 'scissors'){
 
         if(cpuMove === 'rock'){
             result = 'You lost'
+            score.losses++;
 
         } else if (cpuMove === 'paper'){
             result = 'you won';
+            score.wins++;
 
         } else if (cpuMove === 'scissors'){
             result = 'its tie';
+            score.ties++;
         } 
     }
 
-    alert(`You picked rock, Cpu picked ${cpuMove}, result is ${result}`)
+    alert(`You picked rock, Cpu picked ${cpuMove}, result is ${result}
+Wins: ${score.wins}, Losses: ${score.losses},  Ties: ${score.ties}
+    `)
 }
-
-
+function resetGame(){
+    score.losses = 0;
+    score.ties = 0;
+    score.wins= 0;
+}
