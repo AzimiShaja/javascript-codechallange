42ec62fa283c1f48a49703f68a4d17bbd54d1874
Ahmad Shaja AZIMI <110715621+AzimiShaja@users.noreply.github.com>
2023-06-18 11:43:13 +0300

    Update index.js

--- boolean & if-else/index.js
+++ boolean & if-else/index.js
@@ -1,84 +1,89 @@
-const score = JSON.parse(localStorage.getItem('score')) 
-|| {
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+updateScoreElement();
+
+/*
+if (!score) {
+  score = {
     wins: 0,
     losses: 0,
     ties: 0
-};
+  };
+}
+*/
 
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
 
+  let result = "";
 
-function pickComputerMove(){
-    const randomMove = Math.floor(Math.random() * 3 + 1);
-
-    let computerMove = '';
-
-    if ( randomMove == 1){
-        computerMove = 'rock';   
-    } else if (randomMove == 2){
-        computerMove = 'paper';
-    } else if (randomMove == 3){
-        computerMove = 'scissors'
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You lose.";
+    } else if (computerMove === "paper") {
+      result = "You win.";
+    } else if (computerMove === "scissors") {
+      result = "Tie.";
     }
-    return computerMove;
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You win.";
+    } else if (computerMove === "paper") {
+      result = "Tie.";
+    } else if (computerMove === "scissors") {
+      result = "You lose.";
+    }
+  } else if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie.";
+    } else if (computerMove === "paper") {
+      result = "You lose.";
+    } else if (computerMove === "scissors") {
+      result = "You win.";
+    }
+  }
+
+  if (result === "You win.") {
+    score.wins += 1;
+  } else if (result === "You lose.") {
+    score.losses += 1;
+  } else if (result === "Tie.") {
+    score.ties += 1;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+
+  updateScoreElement();
+
+  document.querySelector(".js-result").innerHTML = result;
+
+  document.querySelector(
+    ".js-moves"
+  ).innerHTML = `You ${playerMove} - ${computerMove} Computer`;
 }
 
-function checkWinner(playerMove){
-    let cpuMove = pickComputerMove(); 
-    let result = ' ';
-
-    if ( playerMove === 'rock'){
-
-        if(cpuMove === 'rock'){
-            result = 'Its Tie'
-            score.ties++;
-
-        } else if (cpuMove === 'paper'){
-            result = 'You Lost to cpu';
-            score.losses++;
-
-        } else if (cpuMove === 'scissors'){
-            result = 'You won'
-            score['wins']++;
-        }
-    } else if (playerMove === 'paper'){
-
-        if(cpuMove === 'rock'){
-            result = 'You won'
-            score.wins++;
-
-        } else if (cpuMove === 'paper'){
-            result = 'Its tie';
-            score.ties++;
-
-        } else if (cpuMove === 'scissors'){
-            result = 'You lost to cpu'
-            score.losses++;
-        } 
-    } else if (playerMove === 'scissors'){
-
-        if(cpuMove === 'rock'){
-            result = 'You lost'
-            score.losses++;
-
-        } else if (cpuMove === 'paper'){
-            result = 'you won';
-            score.wins++;
-
-        } else if (cpuMove === 'scissors'){
-            result = 'its tie';
-            score.ties++;
-        } 
-    }
-    
-    localStorage.setItem('score' , JSON.stringify(score));
-
-    alert(`You picked rock, Cpu picked ${cpuMove}, result is ${result}
-Wins: ${score.wins}, Losses: ${score.losses},  Ties: ${score.ties}
-    `)
+function updateScoreElement() {
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
-function resetGame(){
-    score.losses = 0;
-    score.ties = 0;
-    score.wins= 0;
-    localStorage.removeItem('score');
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = "";
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
+  }
+
+  return computerMove;
 }
